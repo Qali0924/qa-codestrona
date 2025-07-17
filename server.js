0551fa82d3743e1ce47723af309ad18fe3e20d94
Qali0924 <qali09@op.pl>
2025-07-17 18:43:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,16 @@ const PORT = 3000;
 
 // Middleware do obsługi formularzy
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public')); // katalog z HTML/CSS
 
+// Statyczne pliki (np. style.css, skrypty JS) - jeśli masz w katalogu 'static'
+// Możesz zmienić 'static' na inny folder lub usunąć tę linię, jeśli nie masz statycznych plików
+// app.use(express.static(path.join(__dirname, 'static')));
+
+// Jeśli statyczne pliki masz w katalogu głównym, możesz udostępnić katalog główny (uwaga na bezpieczeństwo!)
+// Lepiej dać osobny folder, ale jeśli chcesz:
+app.use(express.static(path.join(__dirname))); 
+
+// Obsługa formularza POST
 app.post('/zapisz', (req, res) => {
   const { email, typ, opis } = req.body;
 
@@ -28,14 +36,14 @@ app.post('/zapisz', (req, res) => {
       return res.status(500).send('Błąd serwera');
     }
 
-    // Przekierowanie na stronę główną z info
+    // Przekierowanie na stronę główną z parametrem status=success
     res.redirect('/?status=success');
   });
 });
 
 // Serwowanie strony głównej
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start serwera
